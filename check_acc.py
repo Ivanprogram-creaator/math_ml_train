30f1efeb598147bf884c1d069a4f0b4a9b598be6
Ivan Frolov <ivan-anatoliu@ya.ru>
2025-04-22 18:03:47 +0300

    hf_23

--- check_acc.py
+++ check_acc.py
@@ -6,17 +6,17 @@ import ydf
 from get_data import get_data
 
 train_data, test_data, sharded_train_paths = get_data()
-
+info = {}
 for model in os.listdir("./work_dir/models"):
-    info = {}
+
     name = model
     loaded_model = ydf.load_model("./work_dir/models/" + model)
     info[name] = {"evaluation_test": 0, "evaluation_train": 0}
     info[name]["evaluation_test"] = loaded_model.evaluate(test_data).accuracy * 100
     info[name]["evaluation_train"] = loaded_model.evaluate(train_data).accuracy * 100
-    with open('models_hp.json', mode="r", encoding="utf-8") as models_hp_file:
-        models_hp = json.load(models_hp_file)
-    with open('models_hp.json', mode="w", encoding="utf-8") as models_hp_file:
-        models_hp.append(info)
-        json.dump(models_hp, models_hp_file)
     print(info)
+with open('models_hp.json', mode="r", encoding="utf-8") as models_hp_file:
+    models_hp = json.load(models_hp_file)
+with open('models_hp.json', mode="w", encoding="utf-8") as models_hp_file:
+    models_hp.append(info)
+    json.dump(models_hp, models_hp_file)
