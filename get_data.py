455fb33375254882164d14ff07b273ef80e06c1f
Ivan Frolov <ivan-anatoliu@ya.ru>
2025-04-22 22:24:10 +0300

    hf_28

--- get_data.py
+++ get_data.py
@@ -5,27 +5,13 @@ import pandas as pd
 
 def get_data():
     df = pd.read_excel('data.xlsx')
-    df = df.drop(['Unnamed: 0', 'Unnamed: 1', 'Ситуация'], axis=1)
-    vals = df["Потребности"].values
-    vals = list(map(lambda x: list(map(float, x.split(', '))), vals))
-    df["Номер куба"] = list(map(lambda x: int(x) - 1, df["Номер куба"].values))
-    df["Потребности"] = list(map(lambda x: sum(x), vals))
-    df["0"] = list(map(lambda x: x[0], vals))
-    df["1"] = list(map(lambda x: x[1], vals))
-    df["2"] = list(map(lambda x: x[2], vals))
-    df["3"] = list(map(lambda x: x[3], vals))
-    df["4"] = list(map(lambda x: x[4], vals))
-    df["5"] = list(map(lambda x: x[5], vals))
-    df["6"] = list(map(lambda x: x[6], vals))
-    df = df.dropna()
-    df = df.drop(["Потребности"], axis=1)
-    df.rename(columns={"Номер куба": "Номер_куба"}, inplace=True)
-    df = df.sample(frac=1).reset_index(drop=True)
+    df = df.drop(['Unnamed: 0'], axis=1)
     len_df = len(df.index)
     train_data = df.iloc[df.index <= len_df * 0.8]
     test_data = df.loc[df.index >= len_df * 0.8]
     sharded_train_paths = split_dataset(train_data, "train_data", 10)
     return train_data, test_data, sharded_train_paths
+
 
 def split_dataset(
         dataset: pd.DataFrame, tmp_dir: str, num_shards: int
