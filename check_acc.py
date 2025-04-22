ac9a4b4456f2c5c1b7850fa9d1cfda30ce55d9fc
Ivan Frolov <ivan-anatoliu@ya.ru>
2025-04-22 17:26:18 +0300

    hf_12

--- check_acc.py
+++ check_acc.py
@@ -1,6 +1,5 @@
 import json
 import os
-from datetime import datetime
 
 import ydf
 
@@ -8,11 +7,10 @@ from get_data import get_data
 
 train_data, test_data, sharded_train_paths = get_data()
 info = {}
-for model in os.listdir("work_dir/models/"):
+for model in os.listdir("C:\models\models"):
     name = model
-    loaded_model = ydf.load_model("work_dir/models/" + model)
-    info[name] = {"evaluation_test": 0, "evaluation_train": 0,
-                  "datetime": datetime.now().strftime("%H_%M_%S")}
+    loaded_model = ydf.load_model("C:\models\models/" + model)
+    info[name] = {"evaluation_test": 0, "evaluation_train": 0}
     info[name]["evaluation_test"] = loaded_model.evaluate(test_data).accuracy * 100
     info[name]["evaluation_train"] = loaded_model.evaluate(train_data).accuracy * 100
     with open('models_hp.json', mode="r", encoding="utf-8") as models_hp_file:
