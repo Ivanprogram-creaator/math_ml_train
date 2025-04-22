d77fdb389032dc03132d35006818fe56e8c5d8a1
Ivan Frolov <ivan-anatoliu@ya.ru>
2025-04-22 18:05:23 +0300

    hf_24

--- check_acc.py
+++ check_acc.py
@@ -14,7 +14,7 @@ for model in os.listdir("./work_dir/models"):
     info[name] = {"evaluation_test": 0, "evaluation_train": 0}
     info[name]["evaluation_test"] = loaded_model.evaluate(test_data).accuracy * 100
     info[name]["evaluation_train"] = loaded_model.evaluate(train_data).accuracy * 100
-    print(info)
+    print(info[name])
 with open('models_hp.json', mode="r", encoding="utf-8") as models_hp_file:
     models_hp = json.load(models_hp_file)
 with open('models_hp.json', mode="w", encoding="utf-8") as models_hp_file:
